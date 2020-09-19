18520e47a64a360ac1b67fdda1979c81554bffc2
Gustavo Maciel <gustamaciel2012@hotmail.com>
2020-09-19 15:23:53 -0300

    Update Module1.vb

--- VBConsoleApp/Module1.vb
+++ VBConsoleApp/Module1.vb
@@ -1,7 +1,13 @@
 ﻿Module Module1
 
     Sub Main()
+        Console.Title = "VB Console App example"
+        Console.ForegroundColor = ConsoleColor.DarkGreen
 
+        Console.WriteLine("Welcome to my VB's console application!")
+        Console.WriteLine("Type any key to finish the program.")
+
+        Console.ReadKey()
     End Sub
 
 End Module
